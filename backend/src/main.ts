759e68ad6d0918ce01dd2513bcf240d432f91c46
Marcosdaag <marcosoffs99@gmail.com>
2026-01-04 20:53:54 -0300

    base config nestjs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'; // Importa el modulo inicial app
 
+//Funcion bootstrap, donde creo con NestFactory el servidor y lo abro en el puerto 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+
+//Ejecuto la funcion inicial de arriba bootstrap
 bootstrap();
